7e68e536a2560db57d628565cb1b49dbac645603
Tom Birdsong <tbirdso@titan3.cs.clemson.edu>
2019-09-26 21:10:16 -0400

    Init file

---
+++ synch.c
@@ -0,0 +1,14 @@
+/* Programmer:	Tom Birdsong
+ * Course:	ECE 3220
+ * Date:	Fall 2019
+ * Assignment:	Program 1
+ * Filename:	synch.c
+ * Description: Create producer/consumer logic in multithreaded environment
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello world");
+	return 0;
+}
